563ca14e17e6060f876e92509ac00569d16e4f7c
Nemo <XZC168520@163.com>
2016-11-22 09:11:44 +0800

    first

---
+++ CocoaHTTPServerDemo/IPHelper.h
@@ -0,0 +1,18 @@
+//
+//  IPHelper.h
+//  CocoaHTTPServerDemo
+//
+//  Created by Nemo on 2016/11/19.
+//  Copyright © 2016年 Nemo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IPHelper : NSObject
+
+/*!
+ * get device ip address
+ */
++ (NSString *)deviceIPAdress;
+
+@end
